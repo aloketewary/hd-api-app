76322cc75658f5c01e0503b63d259a8dea9a7444
Aloke Tewary <aloketewary@gmail.com>
2021-07-15 12:55:53 +0530

    Updated and added auth functionality

---
+++ src/test/kotlin/com/hardwaredash/app/hardwaredash/HardwareDashApplicationTests.kt
@@ -0,0 +1,14 @@
+package com.hardwaredash.app.hardwaredash
+
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HardwareDashApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
